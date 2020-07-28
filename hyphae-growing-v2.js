6a0d0297eac64e7691dcc50ccc7ea32dd8b9510b
Ivan Velev <vanecat@gmail.com>
2020-07-28 13:47:46 -0700

    upgraded

--- hyphae-growing-v2.js
+++ hyphae-growing-v2.js
@@ -2,87 +2,147 @@
  * If you don't see anything, then just wait a while.
  *****************************************************/
 
-function HyphaeGrowing() {
-    var hypaeColor = color(250, 250, 250, 1000);
-//var hypaeColor = color(255, 0, 0);
-    stroke(hypaeColor);
+function HyphaeGrowing(width, height, parentEl=false) {
+    const logEl = document.getElementById('log');
 
-// Probability of a turn
-    var pTurn = 0.005;
+    const c = document.createElement('canvas');
+    c.width = width;
+    c.height = height;
+    parentEl = !parentEl ? document.body : parentEl;
 
-// Probability of a split
-    var pSplit = 0.01;
+    parentEl.appendChild(c);
+    const ctx = c.getContext("2d");
 
-// Speed of growth
-    var speed = 0.05;
 
-    var running = true;
+    let isRunning = true;
 
-// Point at which the hyphae grows
-    var Growth = function(x, y, angle) {
-        this.x = x;
-        this.y = y;
-        this.angle = angle;
-
-        this.newCurve = function() {
-            this.curve = (random() * 0.4 + 0.1);
-            if (random < 0.5) {
-                this.curve *= -1;
-            }
-        };
-        this.newCurve();
-
-        this.update = function() {
-            this.angle += this.curve;
-
-            if (random() < pTurn) {
-                this.newCurve();
-            }
-
-            var x = this.x + speed * cos(this.angle);
-            var y = this.y + speed * sin(this.angle);
-
-            if (this.x > 0 && this.x <= 400 &&
-                this.y > 0 && this.y <= 400) {
-                line(x, y, this.x, this.y);
-            }
-
-            this.x = x;
-            this.y = y;
-        };
-
+    const line = (x1, y1, x0,y0) => {
+        ctx.moveTo(x0, y0);
+        ctx.lineTo(x1, y1);
+        ctx.stroke();
     };
 
-    const growingPoints = [new Growth(50, 250, -45)];
+    const isWithinBounds = (x,y) => {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    };
 
-    var draw = function() {
-        if (running) {
-            for (var g in growingPoints) {
-                var p = growingPoints[g];
-                p.update();
-                if (random() < pSplit) {
-                    // Create a new growing point and split the line
-                    var newPoint = new Growth(p.x, p.y, p.angle + 45);
-                    newPoint.curve = -p.curve;
-                    growingPoints.push(newPoint);
-                    p.angle -= 45;
+
+    const pBranchOff = 0.5; // Probability of a new branch forming at tip
+    const growthLength = 20; // length of growth in pixels
+    const growthLengthIncrement = 5; // length of growth in pixels
+    const growthInterval = 100; // Speed of growth
+    const angleDeltaRange = 180, branchMaxCount = 2, branchGrowthMaxAttempts = 20;
+    const pixelPrecision = 100;
+
+    // Point at which the hyphae grows
+    const growthMatrix = {};
+    const Growth = (x=width/2, y=height/2, angle=null) => {
+        if (angle === null) {
+            // initial angle is random
+            angle = Math.floor(Math.random() * 360);
+        }
+
+        const potentialBranchCount = Math.ceil(Math.random() * branchMaxCount);
+        let attemptedBranchCount = 0;
+
+        const grow = () => {
+
+            let x1 = x, y1 = y, newAngle = 0;
+
+            // attempts (i times)
+            let hasGrownBranch = false;
+            for(let ai = 0; ai < potentialBranchCount; ai++) {
+                // the new angle should add be [-angleDelta/2, +angleDelta/2]
+                newAngle = angle + (-1 * (angleDeltaRange/2)) + Math.random() * angleDeltaRange;
+
+                for(let i = growthLengthIncrement; i <= growthLength; i+=growthLengthIncrement) {
+                    x1 = Math.ceil( pixelPrecision * (x + i * Math.cos(newAngle)) ) / pixelPrecision;
+                    y1 = Math.ceil( pixelPrecision * (y + i * Math.sin(newAngle)) ) / pixelPrecision;
+
+                    if (growthMatrix[x1+'_'+y1] || !isWithinBounds(x1, y1)) {
+                        // no growth (bumped into other branch/frame edge on 1st try)
+                        if (i === 1) {
+                            //console.log('hit other growth too soon');
+                            break;
+                        }
+                        // growth possibly up to here only (bumped into another branch/frame edge after 1st try)
+                        else {
+                            //console.log('grew, but not all the way');
+                            hasGrownBranch = true;
+                            break;
+                        }
+                    } else {
+                        hasGrownBranch = true;
+                        growthMatrix[x1+'_'+y1] = true;
+                    }
+                }
+                if (hasGrownBranch) {
+                    break;
+                }
+            }
+            attemptedBranchCount++;
+
+            if (hasGrownBranch) {
+                line(x1, y1, x, y);
+                if (Math.random() < pBranchOff) {
+                    // Create a new growing point from tip of branch and current angle
+                    return Growth(x1, y1, newAngle);
                 }
             }
 
-            while (random() < (growingPoints.length - 5) / 2000) {
-                // Pick a random point
-                var n = random(growingPoints.length);
-                growingPoints.splice(n, 1);
-            }
+            return false;
+        };
+
+        return {
+            grow,
+            isDoneGrowing: () => { return (attemptedBranchCount >= potentialBranchCount); }
         }
-        fill(0);
-        rect(2, 2, 50, 20);
-        fill(160);
-        text(growingPoints.length, 5, 16);
     };
 
-// Spacebar toggles animation
-    const keyPressed = function() {
-        if (keyCode === 32) { running = !running; }
+    const growingBranches = [Growth()];
+
+    let deadBranchesCount = 0;
+    const draw = () => {
+        if (!isRunning) {
+            return;
+        }
+        const newBranches = [];
+        const deadBranches = [];
+        for (let i in growingBranches) {
+            const newBranch = growingBranches[i].grow();
+            if (newBranch) {
+                newBranches.push(newBranch);
+            }
+
+            if (growingBranches[i].isDoneGrowing()) {
+                deadBranches.push(i);
+            }
+        }
+        deadBranchesCount += deadBranches.length;
+        deadBranches.reverse();
+        for (let i of deadBranches) {
+            growingBranches.splice(i,1);
+        }
+        for (let branch of newBranches) {
+            growingBranches.push(branch);
+        }
+
+        isRunning = growingBranches.length > 0;
+
+        logEl.innerHTML = `${isRunning ? 'is':'NOT'} running :: growing branches: ${growingBranches.length}, dead branches: ${deadBranchesCount}, matrix: ${Object.keys(growthMatrix).length}`;
+
+        setTimeout(draw, growthInterval);
+
+
     };
+
+    // Spacebar toggles animation
+    document.body.addEventListener('keypress', (e) => {
+        if (e.keyCode === 32) {
+            isRunning = !isRunning;
+            if (isRunning) {
+                draw();
+            }
+        }
+    });
 }
