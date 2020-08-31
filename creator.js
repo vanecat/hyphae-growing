572d02a0b5def58b3c440c3da7f344555c42e418
Ivan Velev <vanecat@gmail.com>
2020-08-31 15:07:11 -0700

    vue error handler

--- creator.js
+++ creator.js
@@ -211,6 +211,14 @@ function HyphaeGrowingCreator() {
         });
         vueAppConfig.el = appElSelector;
         vueAppConfig.data.hyphaeContainerSelector = hyphaeContainerSelector;
+
+        Vue.config.errorHandler = function (err, vm, info) {
+            // handle error
+            // `info` is a Vue-specific error info, e.g. which lifecycle hook
+            // the error was found in. Only available in 2.2.0+
+            throw Error(err);
+        };
+
         HyphaeGrowingCreator.INSTANCE.vue = new Vue(vueAppConfig);
         isInit = true;
     };
