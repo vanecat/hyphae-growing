f4dd009a58066a339a3bfbb5fac160ee0d599b33
Ivan Velev <vanecat@gmail.com>
2020-08-31 17:22:52 -0700

    updated paths in distributation-package maker script

--- make-dist.sh
+++ make-dist.sh
@@ -1,15 +1,15 @@
 function makeHyphaeGrowingJs {
-    local jsFile="hyphae-growing.js"
+    local jsFile="./dist/hyphae-growing.js"
     echo "Compiling $jsFile";
 
     echo > $jsFile
 
-    cat ../external/vue.js >> $jsFile && echo >> $jsFile
+    cat ./external/vue.js >> $jsFile && echo >> $jsFile
 
-    cat ../pre-init-styles.js >> $jsFile && echo >> $jsFile
+    cat ./pre-init-styles.js >> $jsFile && echo >> $jsFile
 
-    cat ../hyphae-growing.js >> $jsFile && echo >> $jsFile
+    cat ./hyphae-growing.js >> $jsFile && echo >> $jsFile
 
-    cat ../creator.js >> $jsFile && echo >> $jsFile
+    cat ./creator.js >> $jsFile && echo >> $jsFile
 }
 makeHyphaeGrowingJs;
